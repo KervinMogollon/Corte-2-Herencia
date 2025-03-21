e1786304f04fb8d89f06912f1a6040cb1e31e908
KervinM <kervinM1812@gmail.com>
2025-03-20 23:43:33 -0400

    a

---
+++ L25.1.p.C2.GuiaHerencia.Tienda/js/CL_Main.js
@@ -0,0 +1,17 @@
+/*
+Una tienda planea incrementar el 20% del costo para establecer
+el precio de venta de sus artículos (1=ropa, 2=víveres),
+conociendo su nombre y costo. Para la ropa de color rojo
+(1=blanco, 2=azul, 3=rojo) otorgará un 10% de descuento. Los
+víveres no tienen descuento, pero si es un producto básico, sólo
+aplicará un 5% del costo para la ganancia.
+*/
+
+import CL_Ropa from "./CL_Ropa.js";
+import CL_Vivere from "./CL_Vivere.js";
+
+export default class CL_Main{
+    constructor(){
+        let 
+    }
+}
